f327ef44d1104a261feff417e9522195fb2d9bf4
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-28 16:10:24 -0400

    added a prompt that allows user to choose grid size

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector('.grid-container');
 const GRIDCONAINERSIZE = 400;
-let numOfColumnsRows = 16;
+let numOfColumnsRows = chooseGridSize();
 let gridSize = (num) => {return Math.pow(num, 2)};
 
 createGridUnits(gridSize(numOfColumnsRows));
@@ -19,4 +19,8 @@ function properlySizeGrid() {
     gridUnits.forEach(gridUnit => {
         gridUnit.style = `width: ${GRIDCONAINERSIZE/numOfColumnsRows}px; height: ${GRIDCONAINERSIZE/numOfColumnsRows}px;`;
     });
+}
+
+function chooseGridSize () {
+    return parseInt(prompt('Enter the number of rows.'));
 }
