e2141bdc832456ba90fed726be5d4b8411106764
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-28 22:21:58 -0400

    Fixed the sizing bug when resizing a grid

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const gridContainer = document.querySelector('.grid-container');
-const GRIDCONAINERSIZE = 400;
-let numOfColumnsRows = chooseGridSize();
-let gridSize = (num) => {return Math.pow(num, 2)};
+const GRIDCONTAINERSIZE = 400;
+gridContainer.style = `width: ${GRIDCONTAINERSIZE}px; height: ${GRIDCONTAINERSIZE}px;`
+let numOfColumnsRows = 10;
+let gridSize = (num) => {return Math.pow(num, 2);};
+let currentColor = ('#ffffff');
+let gridUnits = [];
+const sizeChanger = document.querySelector('button');
 
 createGridUnits(gridSize(numOfColumnsRows));
+sizeChanger.addEventListener('click', () => {
+    createGridUnits(gridSize(chooseGridSize()));})
 
 function createGridUnits(size) {
+    deletePrevGrid();
     for (let i = 0; i < size; i++) {
         const gridUnit = document.createElement('div');
         gridContainer.appendChild(gridUnit);
         gridUnit.classList.add('grid-unit');
     }
-    properlySizeGrid();
+    gridUnits = document.querySelectorAll('.grid-unit');
+    properlySizeGrid(gridUnits, size);
 }
 
-function properlySizeGrid() {
-    let gridUnits = document.querySelectorAll('.grid-unit');
+function deletePrevGrid() {
+    if (gridUnits.length !== 0) {
+        gridUnits.forEach(gridUnit => {
+            gridUnit.classList.remove('.grid-unit');
+        });
+    }
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function properlySizeGrid(gridUnits, size) {
     gridUnits.forEach(gridUnit => {
-        gridUnit.style = `width: ${GRIDCONAINERSIZE/numOfColumnsRows}px; height: ${GRIDCONAINERSIZE/numOfColumnsRows}px;`;
+        gridUnit.style = 
+        `width: ${GRIDCONTAINERSIZE/Math.sqrt(size)}px; 
+        height: ${GRIDCONTAINERSIZE/Math.sqrt(size)}px;`;
     });
 }
 
